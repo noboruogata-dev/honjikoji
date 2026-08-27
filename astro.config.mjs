32117347d9202f81c9c7e67d8ebfd2629f8b3944
Noboru Ogata <ogatanoboru@NoborunoMacBook-Air.local>
2026-08-27 18:54:46 +0900

    fix: remove unused @astrojs/tailwind import from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://honjikoji.jp',
-  integrations: [tailwind()],
 });
